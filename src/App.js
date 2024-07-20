ee1d92b705eb9d109997e7d0543be29ad6968b44
Júlio Rodrigues <116827694+MegaPlys@users.noreply.github.com>
2024-07-20 12:01:07 -0300

    TL2 - formulário e CSS
    criado Form e o css do formulário

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { BsTrash, BsBookmarkCheck, BsBookmarkFill } from 'react-icons/bs';
 
-const API = "http://localhost:5000"
+const API = "http://localhost:5000";
 
 function App() {
-  const [title, setTitle] = useState("")
-  const [time, setTime] = useState("")
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+
+    //Envio para API
+    console.log(todo)
+
+    setTitle("");
+    setTime("");
+  };
 
 
   return (
@@ -22,11 +40,39 @@ function App() {
       </div>
 
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          
+          <div className="form-control">
+
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input 
+              type="text" 
+              name="title" 
+              placeholder="Título da tarefa" 
+              onChange={(e) => setTitle(e.target.value)} 
+              value={title || ""}
+              required
+            />
+
+            <label htmlFor="time">Duração:</label>
+            <input 
+              type="text" 
+              name="time" 
+              placeholder="Tempo estimado (em horas)" 
+              onChange={(e) => setTime(e.target.value)} 
+              value={time || ""}
+              required
+            />
+
+          </div>
+          <input type="submit" value="Criar tarefa"/>
+        </form>
       </div>
       
       <div className="list-todo">
-        <p>Listas</p>
+        <h2>Lista de Tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
       </div>
     </div>
   );
